b0a3c543da99950acb2c5fab3ad5125bebb569d7
Jason Jackson <jason@cvk.ca>
2011-06-14 13:13:54 -0400

    refactoring

--- project.clj
+++ project.clj
@@ -3,9 +3,13 @@
 
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
+
                  [net.java.dev.jna/jna "3.3.0"]
                  ;[clj-native "0.9.1-SNAPSHOT"]
-                 [clarsec/clarsec "0.0.1-SNAPSHOT"]
+
+                 ;[clarsec/clarsec "0.0.1-SNAPSHOT"]
+                 [org.clojars.jasonjckn/clarsec "0.0.1-SNAPSHOT"]
+
                  [matchure "0.10.1"]
                  ]
   :dev-dependencies [[swank-clojure "1.2.1"]]
